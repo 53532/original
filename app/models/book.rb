f442eeaa69f69bc04edc18f663421e819baa0b1c
53532 <ishikawa.yuko0911@gmail.com>
2019-03-08 09:27:51 +0000

    本の詳細ページ作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   validates :title, presence: true, length: { maximum:255 }
-  validates :authors, presence: true, length: { maximum:255 }
+  validates :author, presence: true, length: { maximum:255 }
   validates :publisher_name, length: { maximum:255 }
   validates :published_date, length: { maximum:255 }
   validates :isbn, presence: true, length: { maximum: 13 }
