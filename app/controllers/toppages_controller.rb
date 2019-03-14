4d4b07aca14c255ea90e700fc10b36caffed67a8
53532 <ishikawa.yuko0911@gmail.com>
2019-03-14 02:55:40 +0000

    ユーザー情報変更ページ

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,7 @@
 class ToppagesController < ApplicationController
   def index
+    if user_signed_in?
+      redirect_to books_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
       post :back
     end
   end
-  resources :users, only:[:show,:edit,:update] 
+  resources :users, only:[:show] 
   
 end
