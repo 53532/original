1dae55805435b3af136d3ec43afb0f30a4954e97
53532 <ishikawa.yuko0911@gmail.com>
2019-03-15 03:51:06 +0000

    図書館登録日を日本時間に修正

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module Tabelib
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
+
+
   end
 
 end
