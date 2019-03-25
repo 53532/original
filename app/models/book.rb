2adbdad3192c1ec9af9ad2243f2d1489c3aed97a
53532 <ishikawa.yuko0911@gmail.com>
2019-03-25 03:26:52 +0000

    バリデーション修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,8 +3,8 @@ class Book < ApplicationRecord
   belongs_to :user,optional: true
   validates :title, presence: true, length: { maximum:255 }
   validates :author, presence: true, length: { maximum:255 }
-  validates :publisher_name, length: { maximum:255 }
-  validates :published_date, length: { maximum:255 }
+  validates :publisher_name, presence: true, length: { maximum:255 }
+  validates :published_date, presence: true, length: { maximum:255 }
   validates :isbn, presence: true, length: { maximum: 13 }
   validates :image_url, presence: true, length: { maximum:255 }
 
